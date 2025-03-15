ca642c2b55d8674aa534fa0ff1d17b7449f758b1
fakequizz <gabidenov.arsen@mail.ru>
2025-03-15 13:57:16 +0500

    a lot of again changes

--- main.py
+++ main.py
@@ -1,37 +1,33 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse  # Добавьте этот импорт
+from fastapi.responses import HTMLResponse  
 from routes.auth import auth_router
 from routes.logs import logs_router
 from routes.policies import policies_router
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import declarative_base
-from fastapi.templating import Jinja2Templates  # Добавьте этот импорт
-from starlette.requests import Request  # Импортируем Request
+from fastapi.templating import Jinja2Templates 
+from starlette.requests import Request  
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-# Подключаем статику и шаблоны
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# Создаем объект для работы с шаблонами
 templates = Jinja2Templates(directory="templates")
 
-# Подключаем маршруты
 app.include_router(auth_router, prefix="/auth", tags=["Auth"])
 app.include_router(logs_router, prefix="/logs", tags=["Logs"])
 app.include_router(policies_router, prefix="/policies", tags=["Policies"])
 
-@app.get("/", response_class=HTMLResponse)  # Теперь это будет работать
+@app.get("/", response_class=HTMLResponse)  
 async def home(request: Request):
-    """Переход на страницу входа"""
     return templates.TemplateResponse("index.html", {"request": request, "error": None})
 
 @app.get("/admin", response_class=HTMLResponse)
 async def admin_dashboard(request: Request):
-    # Render the admin dashboard template
     return templates.TemplateResponse("admin.html", {"request": request})
+
 
 Base = declarative_base()
 
